db4c403a32c9122efa9cc882836dc9836289e59f
calderon123 <arnoldedict342@gmail.com>
2019-05-23 14:18:49 +0800

    redesigned and fixed map

--- app/src/main/java/mobapp/dntsjay/helperfinder/EmployerHomeActivity.java
+++ app/src/main/java/mobapp/dntsjay/helperfinder/EmployerHomeActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,6 +29,9 @@ public class EmployerHomeActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference();
         txtUserName = findViewById(R.id.txtUserName);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
 
@@ -109,6 +114,26 @@ public class EmployerHomeActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.employer_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.logout) {
+            FirebaseAuth.getInstance().signOut();
+            updateUI(null);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
